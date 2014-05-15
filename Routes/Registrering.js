46b7d24b675665e23003a0b8cf4bf71814bede59
Dmytro Morozov <mdmsua@icloud.com>
2014-05-15 14:21:22 +0300

    Missing file

--- Routes/Registrering.js
+++ Routes/Registrering.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var Q = require("Q"),
+var util = require("util"),
+    Q = require("Q"),
     _ = require("underscore"),
     Registration = require("../modules/Registration");
 
@@ -9,14 +10,42 @@ function Registrering(tableService, queueService) {
 }
 
 Registrering.prototype.index = function (req,  res, next) {
-    Q.all([this.registration.peek()]).spread(function (registrations) {
-        res.render("registrering/index", { title: "Registrering", registreringer: registrations });
+    Q.all([this.registration.peek(), this.registration.getJobCategories(), this.registration.getOrganisations()]).spread(function (registrations, jobCategories, organisations) {
+        res.render("registrering/index", { title: "Registrering", registreringer: registrations, jobkategorier: jobCategories, enheder: organisations });
     });
 };
 
 Registrering.prototype.create = function (req,  res, next) {
-    Q.all([this.registration.create(), this.registration.getDrafts(), this.registration.getTemplates()]).spread(function (projects, drafts, templates) {
-        res.render("registrering/ny", { title: "Registrering", projekter: projects, kladder: drafts || [], skabeloner: templates || [] });
+    var jobkategori = req.query.jobkategori,
+        enhed = req.query.enhed,
+        queue = [];
+    queue.push(this.registration.getJobCategories());
+    queue.push(this.registration.getDrafts());
+    queue.push(this.registration.getTemplates());
+    queue.push(this.registration.getLkos());
+    if (enhed) {
+        queue.push(this.registration.getOrganisations());
+    }
+    Q.all(queue).spread(function (jobCategories, drafts, templates, lkos, organisations) {
+        var model = {
+            title: "Registrering",
+            jobkategorier: jobCategories,
+            kladder: drafts || [],
+            skabeloner: templates || [],
+            jobkategori: jobkategori,
+            enhed: enhed,
+            lkos: _.where(lkos, { kode: (_.findWhere(jobCategories, { uuid: jobkategori }) || { lko: 0 }).lko.toString() })
+        };
+        if (organisations) {
+            model.enheder = organisations;
+        }
+//        if (kombo) {
+//            model.projekter = kombo.projects;
+//            model.aktiviteter = kombo.activities;
+//            model.delregnskaber = kombo.accounts;
+//            model.steder = util.isArray(kombo.location) ? kombo.location : [kombo.location];
+//        }
+        res.render("registrering/ny", model);
     }, function (error) {
         next(error);
     });
@@ -32,7 +61,7 @@ Registrering.prototype.project = function (req,  res, next) {
     });
 };
 
-Registrering.prototype.activity = function (req,  res, next) {
+Registrering.prototype.byProjectActivity = function (req,  res, next) {
     this.registration.stuff(req.params.projekt, req.params.kode, function (error, stuff) {
         if (error) {
             next(error);
@@ -93,5 +122,77 @@ Registrering.prototype.send = function (req, res, next) {
         }
     });
 };
+
+Registrering.prototype.forJobCategory = function (req,  res, next) {
+    Q.all([this.registration.getOrganisations(), this.registration.getLkos(), this.registration.getJobCategories()]).spread(function (organizations, lkos, jobCategories) {
+        var jobCategory = _.findWhere(jobCategories, { uuid: req.params.jobkategori });
+        res.json({
+            organizations: organizations.map(function (organization) {
+                return { id: organization.kode, text: organization.navn };
+            }),
+            lkos: lkos.filter(function (lko) {
+                return jobCategory.lko.toString() === lko.kode;
+            }).map(function (lko) {
+                return { id: lko.kode, text: lko.navn };
+            })
+        });
+    }, function (error) {
+        next(error);
+    });
+};
+
+Registrering.prototype.forOrganization = function (req,  res, next) {
+    this.registration.getLocations(req.params.enhed, function (error, locations) {
+        if (error) {
+            next(error);
+        } else {
+            res.json(locations.map(function (location) {
+                return {
+                    id: location.kode,
+                    text: location.navn
+                };
+            }));
+        }
+    });
+};
+
+Registrering.prototype.forLocation = function (req,  res, next) {
+    this.registration.getForLocation(req.params.sted, function (error, kombo) {
+        if (error) {
+            next(error);
+        } else {
+            res.json({
+                projects: kombo.projects.map(function (project) {
+                    return {
+                        id: project.kode,
+                        text: project.navn
+                    };
+                }),
+                activities: kombo.activities.map(function (activity) {
+                    return {
+                        id: activity.kode,
+                        text: activity.navn
+                    };
+                })
+            });
+        }
+    });
+};
+
+Registrering.prototype.account = function (req,  res, next) {
+    this.registration.getAccounts(req.params.projekt, req.params.aktivitet, function (error, accounts) {
+        if (error) {
+            next(error);
+        } else {
+            res.json(accounts.map(function (location) {
+                return {
+                    id: location.kode,
+                    text: location.navn
+                };
+            }));
+        }
+    });
+};
+
 
 module.exports = Registrering;
